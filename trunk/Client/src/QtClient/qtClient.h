efbf6c98ff6d2bda149eef6e77834e05b9bfee57
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2013-08-08 13:30:13 +0000

    Cleanup Icon usage and add edit/disable/enable icons - Add edit icon - split icons for different uses (tool_add -> add + status_new) - Add disable and enable icons for disable button (w/ automatic switch)

--- trunk/Client/src/QtClient/qtClient.h
+++ trunk/Client/src/QtClient/qtClient.h
@@ -104,7 +104,9 @@ private:
 	QMenu *trayIconMenu;
 	QAction *quitAction, *showAction;
 	QTimer *delayTimer;
-	QIcon icon,icon_conn,icon_sync,icon_ul,icon_dl,icon_cf,icon_err,icon_ok,status_sync,status_err,status_done,status_ok,tool_add,tool_remove,lock;
+	QIcon icon,icon_conn,icon_sync,icon_ul,icon_dl,icon_cf,icon_err,icon_ok;
+	QIcon status_sync,status_err,status_done,status_ok,status_new,status_disabled,status_unknown;
+	QIcon lock,enable,disable;
 #ifdef MC_IONOTIFY
 	QIcon act_nofs,act_fs,act_nodb,act_db,act_nosrv,act_srv;
 #endif
