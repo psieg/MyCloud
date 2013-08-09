383e477337f43b0a647a85cbaa7f34ad3b97e90e
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2013-08-09 20:13:27 +0000

    Add Filter management protocol on Client - add constants - add pack_ functions - minor refactoring (const)

--- trunk/Client/src/mycloud/mc_protocol.h
+++ trunk/Client/src/mycloud/mc_protocol.h
@@ -3,34 +3,36 @@
 #include "mc.h"
 /* This unit defines the Codes used in our protocol and functions to pack/unpack messages */
 
-#define MC_CLIENT_PROTOCOL_VERSION		5
+#define MC_CLIENT_PROTOCOL_VERSION		6
 #define MC_MIN_SERVER_PROTOCOL_VERSION	4
 
 typedef int MC_SRVQUERY;
 #define MC_SRVQRY_STATUS		100	// What's your status
 #define MC_SRVQRY_AUTH			101	// Authorize + Timecheck (all following queries require an AuthToken)
 #define MC_SRVQRY_LISTSYNCS		200	// List all your syncs (so I can compare with mine)
-#define MC_SRVQRY_CREATESYNC	201
-#define MC_SRVQRY_UPDATESYNC	202
-#define MC_SRVQRY_DELETESYNC	203
-#define MC_SRVQRY_LISTFILTERS	300
+#define MC_SRVQRY_CREATESYNC	201 // Create new sync
+#define MC_SRVQRY_DELSYNC		202 // Delete sync
+#define MC_SRVQRY_LISTFILTERS	300 // List all filters for a sync
+#define MC_SRVQRY_PUTFILTER		301 // Add / Replace a filter
+#define MC_SRVQRY_DELFILTER		302 // Delete a filter
 #define MC_SRVQRY_LISTDIR		401	// List all files in a dir (with their stats)
 #define MC_SRVQRY_GETFILE		402	// Get the content of a file
 #define MC_SRVQRY_GETMETA		403	// Get a file's metadata
 #define MC_SRVQRY_GETOFFSET		404	// Get the offset of an incomplete file (to complete it)
-#define MC_SRVQRY_PUTFILE		410	// Add/Replace a file
+#define MC_SRVQRY_PUTFILE		410	// Add / Replace a file
 #define MC_SRVQRY_ADDFILE		411	// Add the following content to a file
 #define MC_SRVQRY_PATCHFILE		412	// Patch the metaddata of a file
 #define MC_SRVQRY_DELFILE		413	// Delete a file
 #define MC_SRVQRY_PURGEFILE		420	// Purge this file (it's been hit by an ignore list)
 #define MC_SRVQRY_NOTIYCHANGE	500 // Return when something changes or after timeout
 
-typedef int MC_SRVSTATUS;
+typedef int MC_SRVSTATUS; 
 #define MC_SRVSTAT_OK			100	// I'm good / Query successful
 #define MC_SRVSTAT_AUTHED		101	// QRY_AUTH succeeded, here's your AuthToken
 #define MC_SRVSTAT_SYNCLIST		200	// Here's a list of my syncs
 #define MC_SRVSTAT_SYNCID		201	// ID of sync just created
-#define MC_SRVSTAT_FILTERLIST	300
+#define MC_SRVSTAT_FILTERLIST	300 // List of filters for sync
+#define MC_SRVSTAT_FILTERID		301 // ID of created / updated filter
 #define MC_SRVSTAT_DIRLIST		400	// Here's the directory listing you requested
 #define MC_SRVSTAT_FILE			401	// Here are contents of the file
 #define MC_SRVSTAT_FILEMETA		402	// Metadata of the file
@@ -55,6 +57,8 @@ void pack_code(mc_buf *buf, int code);
 void pack_auth(mc_buf *buf, string user, string passwd);
 void pack_listsyncs(mc_buf *buf, unsigned char authtoken[16]);
 void pack_listfilters(mc_buf *buf, unsigned char authtoken[16], int syncid);
+void pack_putfilter(mc_buf *buf, unsigned char authtoken[16], mc_filter *filter);
+void pack_delfilter(mc_buf *buf, unsigned char authtoken[16], int id);
 void pack_listdir(mc_buf *buf, unsigned char authtoken[16], int parent);
 void pack_getfile(mc_buf *buf, unsigned char authtoken[16], int id, int64 offset, int64 blocksize, unsigned char hash[16]);
 void pack_getoffset(mc_buf *buf, unsigned char authtoken[16], int id);
