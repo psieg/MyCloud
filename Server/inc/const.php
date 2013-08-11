19c2a8211b4b862d0e1bd9068747d9ea2c263962
Patrick Siegler <psieg@users.noreply.github.com>
2013-08-11 18:36:20 +0000

    Switch to protocol 7 because of the hashing (server)

--- Server/inc/const.php
+++ Server/inc/const.php
@@ -7,7 +7,8 @@ define('MC_SYNCSTAT_RUNNING',1); // Not used on server
 define('MC_FILESTAT_COMPLETE',0);
 define('MC_FILESTAT_DELETED',1);
 define('MC_FILESTAT_INCOMPLETE_UP',2);
-define('MC_FILESTAT_INCOMPLETE_DOWN',3); // Not used on server
+define('MC_FILESTAT_INCOMPLETE_UP_ME',3); // Not used on server
+define('MC_FILESTAT_INCOMPLETE_DOWN',4); // Not used on server
 
 define('MC_FILTERT_MATCH_NAME',0);
 define('MC_FILTERT_MATCH_EXTENSION',1);
