aabc456d4a7898d188b8969109763c13089b1c10
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2013-08-11 22:07:52 +0000

    FixRelease 0.2.8

---
+++ tags/Client-0.2.8/src/QtClient/qtClient.h
@@ -0,0 +1,122 @@
+#ifndef QTCLIENT_H
+#define QTCLIENT_H
+#include <QtCore/QTimer>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QSystemTrayIcon>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QMenu>
+#include <QtGui/QCloseEvent>
+#include <QtWidgets/QMessageBox>
+#include "ui_qtClient.h"
+#include "qtConflictDialog.h"
+#include "qtSettingsDialog.h"
+#include "qtSyncDialog.h"
+#include "qtUpdateChecker.h"
+#include "mc_types.h"
+#include "mc_util.h"
+#include "mc_db.h"
+#include "mc_transfer.h"
+
+
+class QtClient : public QMainWindow
+{
+	Q_OBJECT
+signals:
+	void _logOutput(QString s);
+	void _notifyEnd(int evt);
+	void _notifyStart(int evt, QString object);
+//#ifdef MC_IONOTIFY //QtDisallow
+	void _notifyIOEnd(int evt);
+	void _notifyIOStart(int evt);
+//#endif
+	void _notifyProgress(int value, int total);
+	void _notifySubProgress(double value, double total);
+
+public:	
+	QtClient(QWidget *parent = 0, int autorun = 0);
+	~QtClient();
+	static QtClient* instance(QWidget *parent = 0){/* if(!_instance) _instance = new QtClient(parent);*/ return _instance; }
+
+	void logOutput(QString s);
+	void notifyEnd(int evt);
+	void notifyStart(int evt, std::string object);
+#ifdef MC_IONOTIFY
+	void notifyIOEnd(int evt);
+	void notifyIOStart(int evt);
+#endif
+	void notifyProgress(int value, int total);
+	void notifySubProgress(double value, double total);
+	static int execConflictDialog(std::string *fullPath, std::string *descLocal, std::string *descServer, int defaultValue, bool isDir, bool manualSolvePossible);
+
+	int listSyncs();
+	void updateStatus(QString object);
+	void setStatus(QString prefix, QString object, QIcon icon, bool wicon = true);
+
+protected:
+	void showEvent(QShowEvent *event);
+	void hideEvent(QHideEvent *event);
+	void closeEvent(QCloseEvent *event);
+
+public slots:
+	void startNewRun();
+	void quit();
+
+private slots:
+	void __logOutput(QString s);
+	void __notifyEnd(int evt);
+	void __notifyStart(int evt, QString object);
+//#ifdef MC_IONOTIFY //Qt disallow
+	void __notifyIOEnd(int evt);
+	void __notifyIOStart(int evt);
+//#endif
+	void __notifyProgress(int value, int total);
+	void __notifySubProgress(double value, double total);
+	int _execConflictDialog(std::string *fullPath, std::string *descLocal, std::string *descServer, int defaultValue, bool isDir, bool manualSolvePossible);
+	void on_quitButton_clicked();
+	void on_pushButton_clicked();
+	void on_pushButton2_clicked();
+	void on_addButton_clicked();
+	void on_removeButton_clicked();
+	void on_upButton_clicked();
+	void on_downButton_clicked();
+	void on_editButton_clicked();
+	void on_settingsButton_clicked();
+	void on_disableButton_clicked();
+	void on_syncTable_itemSelectionChanged();
+	void trayIconActivated(QSystemTrayIcon::ActivationReason reason);
+	void newVersion(QString newver);
+	
+private:
+	static QtClient *_instance;
+	Ui::QtClient ui;
+	QtWorkerThread worker;
+	QtUpdateChecker updateChecker;
+	QtConflictDialog *conflictDialog;
+	QLabel *statusLabel;
+#ifdef MC_IONOTIFY
+	QLabel *fsLabel,*dbLabel,*srvLabel;
+#endif
+	QProgressBar *progressBar;
+	QLabel *progressLabel;
+	QSystemTrayIcon *trayIcon;
+	QMenu *trayIconMenu;
+	QAction *quitAction, *showAction;
+	QTimer *delayTimer;
+	QIcon icon,icon_conn,icon_sync,icon_ul,icon_dl,icon_cf,icon_err,icon_ok;
+	QIcon status_sync,status_err,status_done,status_ok,status_new,status_disabled,status_unknown;
+	QIcon lock,enable,disable;
+#ifdef MC_IONOTIFY
+	QIcon act_nofs,act_fs,act_nodb,act_db,act_nosrv,act_srv;
+#endif
+	QString currentPrefix,currentConnectString,currentSyncString,currentFileString;
+	float charWidth;
+	bool uploading,downloading;
+	int64 progressBase,progressTotal;
+	std::vector<mc_sync_db> synclist;
+
+
+
+};
+
+#endif // QTCLIENT_H
---
+++ tags/Client-0.2.8/src/QtClient/qtConflictDialog.cpp
@@ -0,0 +1,104 @@
+#include "qtConflictDialog.h"
+
+QtConflictDialog::QtConflictDialog(QWidget *parent)
+	: QDialog(parent)
+{
+	quit = false;
+	ui.setupUi(this);
+	setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+	quittimer.setSingleShot(false);
+	quittimer.setInterval(2000);
+	quittimer.start();
+	connect(&quittimer,SIGNAL(timeout()),this,SLOT(checkquit()));
+}
+
+QtConflictDialog::~QtConflictDialog()
+{
+
+}
+
+int QtConflictDialog::exec(std::string *fullPath, std::string *descLocal, std::string *descServer, int defaultValue, bool isDir, bool manualSolvePossible)
+{
+	setWindowTitle(tr("Conflict: ") + fullPath->c_str());
+	ui.downloadButton->setDescription(descServer->c_str());
+	ui.uploadButton->setDescription(descLocal->c_str());
+	ui.keepButton->setEnabled(manualSolvePossible);
+	ui.dirBox->setChecked(false);
+	ui.recursiveBox->setChecked(false);
+	ui.recursiveBox->setEnabled(false);
+	switch(defaultValue){
+		case 1:
+			ui.uploadButton->setDefault(true);
+			ui.uploadButton->setFocus();
+			break;
+		case 2:
+			ui.downloadButton->setDefault(true);
+			ui.downloadButton->setFocus();
+			break;
+		default:
+			if(manualSolvePossible){
+				ui.keepButton->setDefault(true);
+				ui.keepButton->setFocus();
+			} else {
+				ui.skipButton->setDefault(true);
+				ui.skipButton->setFocus();
+			}
+	}
+	return QDialog::exec();
+}
+
+void QtConflictDialog::closeEvent(QCloseEvent *event)
+{
+	//event->ignore();
+	done(Skip);
+}
+
+void QtConflictDialog::keyPressEvent(QKeyEvent *e) {
+    //if(e->key() != Qt::Key_Escape) //No one may close or ignore us, this choice needs to be made
+        QDialog::keyPressEvent(e);
+
+}
+
+void QtConflictDialog::reject(){
+	done(Skip);
+}
+
+void QtConflictDialog::checkquit(){
+	if(quit){
+		done(Terminating);
+		qApp->quit();
+	}
+	if(MC_TERMINATING()){
+		done(Terminating);
+	}
+}
+
+void QtConflictDialog::on_downloadButton_clicked()
+{
+	if(ui.dirBox->isChecked())
+		if(ui.recursiveBox->isChecked()) done(DownloadDR);
+		else done(DownloadD);
+	else 
+		done(Download);
+}
+
+void QtConflictDialog::on_uploadButton_clicked()
+{
+	if(ui.dirBox->isChecked())
+		if(ui.recursiveBox->isChecked()) done(UploadDR);
+		else done(UploadD);
+	else 
+		done(Upload);
+}
+
+void QtConflictDialog::on_keepButton_clicked()
+{
+	if(ui.dirBox->isChecked()) done(KeepD);
+	else done(Keep);
+	
+}
+void QtConflictDialog::on_skipButton_clicked()
+{
+	if(ui.dirBox->isChecked()) done(SkipD);
+	else done(Skip);
+}
