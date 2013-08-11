4687527fd4d0e58a38f613fcbf9f792dd49946d6
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2013-08-11 18:54:18 +0000

    More missed merging, going to version 0.2.8

--- trunk/Client/src/mycloud/mc_transfer.cpp
+++ trunk/Client/src/mycloud/mc_transfer.cpp
@@ -306,7 +306,11 @@ int download(mc_sync_ctx *ctx, const string& path, mc_file_fs *fs, mc_file *db, 
 
 	if(srv->status == MC_FILESTAT_INCOMPLETE_UP){
 		MC_INF("Not downloading file " << srv->id << ": " << printname(srv) << ", file is not complete");
-		if(db) crypt_filestring(ctx,db,hashstr); //We use db to have a hash mismatch -> no fullsync (see #3)
+		if(db) rc = db_update_file(srv);
+		else rc = db_insert_file(srv);
+		MC_CHKERR(rc);
+		if(db) crypt_filestring(ctx,db,hashstr);
+		else crypt_filestring(ctx,srv,hashstr);
 		return MC_ERR_INCOMPLETE_SKIP;
 		//TODO: Partial download?
 
@@ -440,7 +444,7 @@ int upload_new(mc_sync_ctx *ctx, const string& path, const string& fpath, const 
 	newdb->is_dir = fs->is_dir;
 	newdb->parent = parent;
 	if(fs->is_dir) newdb->status = MC_FILESTAT_COMPLETE;
-	else newdb->status = MC_FILESTAT_INCOMPLETE_UP;
+	else newdb->status = MC_FILESTAT_INCOMPLETE_UP_ME;
 
 	if(!modified){ // File not modified
 			newdb->status = MC_FILESTAT_COMPLETE;
@@ -585,7 +589,7 @@ int upload_normal(mc_sync_ctx *ctx, const string& path, const string& fpath, con
 				db->mtime = fs->mtime;
 				db->ctime = fs->ctime;
 				db->size = fs->size;
-				db->status = MC_FILESTAT_INCOMPLETE_UP;
+				db->status = MC_FILESTAT_INCOMPLETE_UP_ME;
 
 							
 				rc = crypt_filemd5_new(&cctx,db->hash,fpath,db->size);
@@ -623,7 +627,7 @@ int upload_normal(mc_sync_ctx *ctx, const string& path, const string& fpath, con
 					rc = srv_delfile(db);
 					MC_CHKERR(rc);
 							
-					db->status = MC_FILESTAT_INCOMPLETE_UP;
+					db->status = MC_FILESTAT_INCOMPLETE_UP_ME;
 					rc = crypt_filemd5_new(&cctx,db->hash,fpath,db->size);
 					MC_CHKERR(rc);	
 					//db->mtime = time(NULL);
@@ -652,7 +656,7 @@ int upload_normal(mc_sync_ctx *ctx, const string& path, const string& fpath, con
 		db->ctime = fs->ctime;
 		db->mtime = fs->mtime;
 		db->size = fs->size;
-		db->status = MC_FILESTAT_INCOMPLETE_UP;
+		db->status = MC_FILESTAT_INCOMPLETE_UP_ME;
 		memcpy(db->hash,hash,16);
 
 
