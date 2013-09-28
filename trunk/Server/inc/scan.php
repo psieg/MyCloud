dcbc777a6daff73edaf1726d6af1a0a0ada858bf
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2013-09-28 23:14:24 +0000

    Add a .crypted-file if the sync is encrypted (closes #44)

--- trunk/Server/inc/scan.php
+++ trunk/Server/inc/scan.php
@@ -64,10 +64,11 @@ function sscan($uid,$path){
 	$l = scandir($path);
 	foreach($l as $s){
 		if(is_dir($path.'/'.$s) && $s != "." && $s != ".."){
-			echo "INSERT INTO mc_syncs (uid,name,filterversion,shareversion,crypted) VALUES ('".$uid."','".esc($s)."',1,1,0)"."\n";
-			$q = $mysqli->query("INSERT INTO mc_syncs (uid,name,filterversion,shareversion,crypted) VALUES ('".$uid."','".esc($s)."',1,1,0)");
+			$cr = file_exists($path.'/'.$s.".crypted");
+			echo "INSERT INTO mc_syncs (uid,name,filterversion,shareversion,crypted) VALUES ('".$uid."','".esc($s)."',1,1,".($cr?1:0).")"."\n";
+			$q = $mysqli->query("INSERT INTO mc_syncs (uid,name,filterversion,shareversion,crypted) VALUES ('".$uid."','".esc($s)."',1,1,".($cr?1:0).")");
 			if(!$q){ echo "Error: ".$mysqli->error; break; }
-			echo "Sync added: ".$s." (".$mysqli->insert_id.")\n";
+			echo ($cr?"Encrypted ":"")."Sync added: ".$s." (".$mysqli->insert_id.")\n";
 			//fscan($uid,-$mysqli->insert_id,$path.'/'.$s);
 		}
 	}
