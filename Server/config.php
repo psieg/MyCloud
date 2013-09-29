8b76aaa022e1a316ee31ee090f0a86780ff6365d
Patrick Siegler <psieg@users.noreply.github.com>
2013-09-29 13:02:08 +0000

    Increase token regen time

--- Server/config.php
+++ Server/config.php
@@ -7,5 +7,5 @@ define('MC_BUFSIZE',1024*1024); // 1MB = ca. 1sec at avg speed
 define('MC_MAXBUFSIZE',1024*1024);
 
 define('MC_SESS_EXPIRE',300); // How long inactive until a new auth is neede
-define('MC_TOKEN_REGEN',7200); // Maximum time one token is valid
+define('MC_TOKEN_REGEN',21600); // Maximum time one token is valid
 ?>
