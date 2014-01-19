4e506877e3ed9c72e4f8c86a2d683dd4f28be022
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2014-01-19 20:54:17 +0000

    Increase maxbufsize to 10M

--- trunk/Server/config.php
+++ trunk/Server/config.php
@@ -3,9 +3,9 @@ define('MC_DB_USER',"www");
 define('MC_DB_PASS',"linuxserverwww");
 define('MC_DB_DATABASE',"cloud");
 define('MC_FS_BASEDIR',"/www/cloud/data");
-define('MC_BUFSIZE',1024*1024); // 1MB = ca. 1sec at avg speed
-define('MC_MAXBUFSIZE',1024*1024);
+define('MC_BUFSIZE',1024*1024); // size of blocks used when receiving 
+define('MC_MAXBUFSIZE',10*1024*1024); // size of block used when sending
 
-define('MC_SESS_EXPIRE',300); // How long inactive until a new auth is neede
+define('MC_SESS_EXPIRE',605); // How long inactive until a new auth is neede
 define('MC_TOKEN_REGEN',21600); // Maximum time one token is valid
 ?>
