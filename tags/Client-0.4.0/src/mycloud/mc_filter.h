0b4a57ea8820e1cc941a8cb03bf864a06a4cec83
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2015-02-19 19:47:58 +0000

    Release 0.4.0

---
+++ tags/Client-0.4.0/src/mycloud/mc_filter.h
@@ -0,0 +1,18 @@
+#ifndef MC_FILTER_H
+#define MC_FILTER_H
+#include "mc.h"
+#include "mc_transfer.h" //for purging invalid files
+
+#include <regex>
+
+// Functions for filtering directory listings, so we can ignore files
+/* update filter lists for sync from server	*/
+int update_filters(int sid, list<mc_filter> *l);
+
+/* match the full path against all filters, returns whether hit	*/
+bool match_full(const string& path, mc_file *file, list<mc_filter> *filter);
+bool match_full(const string& path, mc_file_fs *file, list<mc_filter> *filter);
+/* filter the listings of files we want to ignore */
+int filter_lists(const string& path, list<mc_file_fs> *onfs, list<mc_file> *indb, list<mc_file> *onsrv, list<mc_filter> *filter);
+
+#endif /* MC_FILTER_H */
