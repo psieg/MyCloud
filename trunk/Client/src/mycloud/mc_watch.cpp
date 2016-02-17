450f01d6ed6ef4bbf47d47cf9ecfc86162266c40
padi <padi@7bc15de8-3aec-4d4d-bd52-68481c4bc4ec>
2016-02-17 06:31:04 +0000

    Fix watchmode causing wrong encryption IV

--- trunk/Client/src/mycloud/mc_watch.cpp
+++ trunk/Client/src/mycloud/mc_watch.cpp
@@ -88,7 +88,7 @@ void QtLocalWatcher::pathChanged(const string& path) {
 #else
 void QtLocalWatcher::pathChanged(const QString& path) {
 	string dirpath(qPrintable(path));
-	if (dirpath.endsWith('/')) {
+	if (dirpath.substr(dirpath.length() - 2) == '/') {
 		dirpath = dirpath.substr(0, dirpath.length() - 1);
 	}
 #endif
